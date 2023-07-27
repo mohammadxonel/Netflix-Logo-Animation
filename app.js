76d882a591ad7542ca08c60d1b955d4681509edc
mohammadxxali <ali_mohammad@t-online.de>
2023-07-27 22:50:36 +0200

    add js app edits const N

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 // --- NETFLIX ANIMATION --- //
 
+/* --- N --- */
+ 
+const n_tl = gsap.timeline();
+ n_tl
+   .to("#N1-shadow", {opacity: 0, duration: 0.5}, 0.3)
+   .to("#N3-shadow", {opacity: 0, duration: 1.5}, 0.3)
+   
+ 
+ 
+/**/
